63d45deac9ad2bc11305adf953f16f24a73e6ada
KyungUp Won <jack@netsarang.com>
2022-06-28 18:12:35 +0900

    add wait-for-it

--- src/web/docker-entrypoint.sh
+++ src/web/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Server is running, please wait..."
+set -e
 
-# sleep infinity
-npm install && npm run start
+wait-for-it.sh 172.21.0.2:3306 -t 30
+
+echo "Server is running, please wait..."
+cd /app && npm install && npm run start
